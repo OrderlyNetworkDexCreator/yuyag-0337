f60b6165b2c1e10e4715d7ae41b10e5757602c75
Mario Reder <mario.reder@pm.me>
2026-04-21 04:08:01 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__RUNTIME_CONFIG__ = {
   "VITE_ORDERLY_BROKER_ID": "demo",
-  "VITE_ORDERLY_BROKER_NAME": "yuyag",
+  "VITE_ORDERLY_BROKER_NAME": "pwnedBy0xf",
   "VITE_BROKER_EOA_ADDRESS": "0xc7dcb77a24e110b5c01de24fd4173548aed15524",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "false",
@@ -14,7 +14,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_DISABLE_EVM_WALLETS": "false",
   "VITE_DISABLE_SOLANA_WALLETS": "false",
   "VITE_WALLETCONNECT_PROJECT_ID": "",
-  "VITE_APP_NAME": "yuyag",
+  "VITE_APP_NAME": "pwnedBy0xf",
   "VITE_APP_DESCRIPTION": "Orderly Trading Application",
   "VITE_HAS_PRIMARY_LOGO": "false",
   "VITE_HAS_SECONDARY_LOGO": "false",
